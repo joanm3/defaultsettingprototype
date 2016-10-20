2a3f17dc43f9e55a58c306dbb6cd34b5a2ed16d8
Joan Mitjans <Joan Mitjans>
2016-10-20 09:03:47 +0200

    Added types of cubes. Raining and scalable cubes.

---
+++ Assets/Scripts/MakeRainCube.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeRainCube : MonoBehaviour {
+
+    public int sumValue = 5;
+    public int min =0;
+    public int max = 10;
+    ParticleSystem particles; 
+    CapsuleCollider playerCollider;
+
+    void Start()
+    {
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<CapsuleCollider>();
+        particles = GetComponentInChildren<ParticleSystem>(); 
+    }
+
+    void FixedUpdate()
+    {
+        sumValue = Mathf.Clamp(sumValue, min, max); 
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other == playerCollider)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                particles.emissionRate += sumValue; 
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                particles.emissionRate -= sumValue;
+
+            }
+        }
+
+    }
+}
---
+++ Assets/changeSizeButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeSizeButton : MonoBehaviour
+{
+
+    public float sumValue = 0.1f;
+    public float min = 0.1f;
+    public float max = 2f;
+    CapsuleCollider playerCollider;
+
+    void Start()
+    {
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<CapsuleCollider>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.localScale = new Vector3(Mathf.Clamp(transform.localScale.x, min, max), Mathf.Clamp(transform.localScale.x, min, max), Mathf.Clamp(transform.localScale.x, min, max));
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other == playerCollider)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                transform.localScale += new Vector3(sumValue, sumValue, sumValue);
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                transform.localScale -= new Vector3(sumValue, sumValue, sumValue);
+            }
+        }
+
+    }
+}
